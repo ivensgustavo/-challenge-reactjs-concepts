5488a8c359f167e232b015c7e3747437bd6aaf32
ivensgustavo <gustavo_ivens@alu.ufc.br>
2020-04-29 17:53:15 -0300

    Challenge solution

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() =>{
+    api.get('repositories').then(response =>{
+      setRepositories(response.data);
+    });
+  }, [repositories]);
+
   async function handleAddRepository() {
-    // TODO
+    const repositorie = {
+      title: `New Repositorie ${Date.now()}`,
+      url: 'https://github.com/ivensgustavo/SistemaGestaoEspacoOtica',
+      techs: ['ReactJS', 'React Native', 'Node.js']
+    }
+
+    const response = await api.post('repositories', repositorie);
+    
+    setRepositories([...repositories, response.data]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`);
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map((repositorie) =>{
+            return (
+              <li key = {repositorie.id}>
+                {repositorie.title}
+                <button onClick={() => handleRemoveRepository(repositorie.id)}>
+                  Remover
+                </button>
+              </li>
+            );
+          })}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
